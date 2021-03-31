3db4b1b4b86d3eb12458e4c3a5cc371627577a31
RockyGracie <igorferreira.bjj@gmail.com>
2021-03-31 11:04:49 -0300

    Added POST request for create new blog post and redirect to home page

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,22 +1,81 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 const NewPost = () => {
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [body, setBody] = useState('');
+  const [isPosting, setIsPosting] = useState(false);
+
+  const history = useHistory();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setIsPosting(true);
+
+    const blog = { title, author, body }
+
+    fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      setIsPosting(false);
+      history.push('/');
+    });
+  };
+
   return (
     <>
       <h2 className="title">Create a New Post</h2>
-      <form className="form">
+      <form
+        className="form"
+        onSubmit={submitHandler}
+      >
         <div className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" />
+          <input
+            required
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="author">Author</label>
-          <input type="text" id="author" />
+          <input
+            required
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="body">Body</label>
-          <textarea id="body" cols="30" rows="10" />
+          <textarea
+            required
+            id="body"
+            cols="30"
+            rows="10"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
         </div>
         <div className="form-control--button">
-          <button type="submit">Add Post</button>
+          {!isPosting ? (
+            <button
+              type="submit"
+            >Add Post</button>
+          ) : (
+            <button
+              type="submit"
+              className="posting"
+            >Posting...</button>
+          )}
         </div>
       </form>
     </>
