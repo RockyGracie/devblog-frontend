91e0bdedf511e5fa4d2543854816a7d66177844f
RockyGracie <igorferreira.bjj@gmail.com>
2021-03-30 21:00:03 -0300

    Created Add New Post page

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -3,7 +3,21 @@ const NewPost = () => {
     <>
       <h2 className="title">Create a New Post</h2>
       <form className="form">
-        <p>ihaaa</p>
+        <div className="form-control">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="author">Author</label>
+          <input type="text" id="author" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Body</label>
+          <textarea id="body" cols="30" rows="10" />
+        </div>
+        <div className="form-control--button">
+          <button type="submit">Add Post</button>
+        </div>
       </form>
     </>
   );
